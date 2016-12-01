bf83603533a8dca48c591985167c506a77e58617
James Bursa <james@zamez.org>
2016-12-01 16:29:08 -0500

    Fix compatibility issues with reading long_description from README (failed on macOS Python 3 with UnicodeDecodeError).

--- setup.py
+++ setup.py
@@ -4,7 +4,12 @@ setup(
     name="joblog",
     version="1.0.2",
     description="Standardized logging for batch jobs and others.",
-    long_description=open("README.md").read(),
+    long_description="""The Joblog library provides a simple logging system.
+    It’s ideal for batch jobs but can also be used for longer running systems.
+    * Log format compatible with [Google Logging Library](https://github.com/google/glog).
+    * Log files are automatically written to a directory tree arranged by date with unique names.
+    * Available for several programming languages.
+    * Status lines logged automatically at start and exit.""",
     author="James Bursa",
     author_email="james@zamez.org",
     url="https://github.com/jamesbursa/joblog",
